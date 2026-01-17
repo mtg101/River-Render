c1772ea9f96abd64280c1fb319062a6dc6cc2d78
Russell Bulmer <russell.bulmer@gmail.com>
2026-01-17 01:03:24 +0000

    black on black bridge to render into

--- rr_game_screen.asm
+++ rr_game_screen.asm
@@ -120,11 +120,10 @@ GAME_CLEAR_RIVER_PIXEL_LOOP:
 	LD 		A, %00001111			; white on blue river
 	LD 		C, %00100001			; blue on green bank
 
-
 	; get row 0 + 8 addr
 	LD 		HL, ATTR_START + 8
 
-	LD 		B, 24 					; 24 attr rows
+	LD 		B, 21 					; 24 attr rows, 3 custom
 	LD 		DE, 17					; 16 along, plus one to avoid an INC :)
 
 GAME_CLEAR_RIVER_ATTR_LOOP_REAL:
@@ -168,6 +167,125 @@ GAME_CLEAR_RIVER_ATTR_LOOP_REAL:
 
 	DJNZ 	GAME_CLEAR_RIVER_ATTR_LOOP_REAL
 
+	; special rows
+	LD 		B, %00000000			; black on black bridge (no loop)
+
+	; 1/3
+	; LD 16 attrs
+	LD 		(HL), C
+	INC 	HL
+	LD 		(HL), B
+	INC 	HL
+	LD 		(HL), B
+	INC 	HL
+	LD 		(HL), A
+	INC 	HL
+
+	LD 		(HL), A
+	INC 	HL
+	LD 		(HL), A
+	INC 	HL
+	LD 		(HL), A
+	INC 	HL
+	LD 		(HL), B
+	INC 	HL
+
+	LD 		(HL), B
+	INC 	HL
+	LD 		(HL), A
+	INC 	HL
+	LD 		(HL), A
+	INC 	HL
+	LD 		(HL), A
+	INC 	HL
+
+	LD 		(HL), A
+	INC 	HL
+	LD 		(HL), B
+	INC 	HL
+	LD 		(HL), B
+	INC 	HL
+	LD 		(HL), C
+
+	ADD 	HL, DE
+
+	; 2/3
+
+	; LD 16 attrs
+	LD 		(HL), C
+	INC 	HL
+	LD 		(HL), B
+	INC 	HL
+	LD 		(HL), B
+	INC 	HL
+	LD 		(HL), B
+	INC 	HL
+
+	LD 		(HL), B
+	INC 	HL
+	LD 		(HL), B
+	INC 	HL
+	LD 		(HL), B
+	INC 	HL
+	LD 		(HL), B
+	INC 	HL
+
+	LD 		(HL), B
+	INC 	HL
+	LD 		(HL), B
+	INC 	HL
+	LD 		(HL), B
+	INC 	HL
+	LD 		(HL), B
+	INC 	HL
+
+	LD 		(HL), B
+	INC 	HL
+	LD 		(HL), B
+	INC 	HL
+	LD 		(HL), B
+	INC 	HL
+	LD 		(HL), C
+
+	ADD 	HL, DE
+
+	; 3/3
+
+	; LD 16 attrs
+	LD 		(HL), C
+	INC 	HL
+	LD 		(HL), B
+	INC 	HL
+	LD 		(HL), B
+	INC 	HL
+	LD 		(HL), B
+	INC 	HL
+
+	LD 		(HL), B
+	INC 	HL
+	LD 		(HL), B
+	INC 	HL
+	LD 		(HL), B
+	INC 	HL
+	LD 		(HL), B
+	INC 	HL
+
+	LD 		(HL), B
+	INC 	HL
+	LD 		(HL), B
+	INC 	HL
+	LD 		(HL), B
+	INC 	HL
+	LD 		(HL), B
+	INC 	HL
+
+	LD 		(HL), B
+	INC 	HL
+	LD 		(HL), B
+	INC 	HL
+	LD 		(HL), B
+	INC 	HL
+	LD 		(HL), C
 
 
 	RET 							; GAME_CLEAR_RIVER
